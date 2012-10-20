89c27a666eacc904de2184d59271033e4ad2a5bb
Tomasz Gieniusz <tomasz.gieniusz@gmail.com>
2012-10-20 10:44:25 +0200

    CLI locale setting

--- lib/git_stats/cli.rb
+++ lib/git_stats/cli.rb
@@ -3,9 +3,11 @@ require "git_stats"
 class GitStats::CLI
 
   def start(*args)
-    raise "Wrong number of arguments\nUsage: git_stats repo_path output_path" unless args.size == 2
+    raise "Wrong number of arguments\nUsage: git_stats repo_path output_path <output_lang>" unless [2, 3].include? args.size
 
-    repo_path, out_path = args
+    repo_path, out_path, lang = args
+    I18n.locale = lang || :en
+
     GitStats::Generator.new(repo_path, out_path) { |g|
       g.add_command_observer { |command, result| puts "#{command}" }
     }.render_all

116cc5a8f4fe3d8199b0cf3c732da0d5729c0d71
Tomasz Gieniusz <tomasz.gieniusz@gmail.com>
2012-10-24 15:59:49 +0200

    fixed encoding problem with some repositories

--- lib/git_stats/git_data/command_runner.rb
+++ lib/git_stats/git_data/command_runner.rb
@@ -3,6 +3,11 @@ module GitStats
   module GitData
     class CommandRunner
       def run(path, command)
+        execute(command, path).encode!('UTF-8', 'UTF-8', :invalid => :replace)
+      end
+
+      private
+      def execute(command, path)
         Dir.chdir(path) { %x[#{command}] }
       end
     end

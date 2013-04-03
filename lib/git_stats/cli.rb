8f67bcdc6432c8ebb7bc93c823a89889d83c2aca
Nick Brown <nickbroon@gmail.com>
2013-04-03 12:35:12 +0100

    Add commit range to the CLI
    This adds 4th and 5th optional CLI parameters that specifies the first
    and last SHA commit to generate stats between.
    The generator and repo classes were changed to take these as
    initialisation parameters.
    It would be nice to have the CLI take individual named optional
    parameters, so that only first or last commit could be given and
    without having to give the language.

--- lib/git_stats/cli.rb
+++ lib/git_stats/cli.rb
@@ -4,12 +4,12 @@ require "git_stats"
 class GitStats::CLI
 
   def start(*args)
-    raise "Wrong number of arguments\nUsage: git_stats repo_path output_path <output_lang>" unless [2, 3].include? args.size
+    raise "Wrong number of arguments\nUsage: git_stats repo_path output_path <output_lang>" unless [2, 3, 4, 5].include? args.size
 
-    repo_path, out_path, lang = args
+    repo_path, out_path, lang, first_commit_sha, last_commit_sha = args
     I18n.locale = lang || :en
 
-    GitStats::Generator.new(repo_path, out_path) { |g|
+    GitStats::Generator.new(repo_path, out_path, first_commit_sha, last_commit_sha) { |g|
       g.add_command_observer { |command, result| puts "#{command}" }
     }.render_all
   end

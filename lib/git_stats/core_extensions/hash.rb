b5da25d52c3938a4d1fa18960962384cbbbc8166
Tomasz Gieniusz <tomasz.gieniusz@gmail.com>
2013-08-09 15:18:31 +0200

    [closes #16] days without commits are not ommited on date charts anymore

--- lib/git_stats/core_extensions/hash.rb
+++ lib/git_stats/core_extensions/hash.rb
@@ -6,4 +6,19 @@ class Hash
     default = params[:default]
     inject(Array.new(min_size, default)) { |acc, (k, v)| acc[k] = v; acc }.map { |e| e || default }
   end
+
+  def fill_empty_days!(params = {:aggregated => false})
+    return self if self.empty?
+
+    days_with_data = self.keys.map(&:to_date).sort.uniq
+    prev = 0
+    days_with_data.first.upto(days_with_data.last) do |day|
+      if days_with_data.include? day
+        prev = self[day]
+      else
+        self[day] = params[:aggregated] ? prev : 0
+      end
+    end
+    self
+  end
 end
